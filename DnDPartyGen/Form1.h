08dee54986bba1d6d6ad64cb8eeb2854e1833c20
GrosJambon1243 <kevindurocher@live.ca>
2024-02-19 20:39:18 -0500

    Button for Menu

--- DnDPartyGen/Form1.h
+++ DnDPartyGen/Form1.h
@@ -35,6 +35,8 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 	protected:
 
 	private:
@@ -51,22 +53,44 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(424, 51);
+			this->button1->Location = System::Drawing::Point(12, 92);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(100, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Party Manager";
 			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 349);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(100, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Exit";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 131);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(100, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Character Creator";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(696, 405);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
