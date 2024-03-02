e993a5b6468bc3a9cc263225f784e2bece3673f1
GrosJambon1243 <kevindurocher@live.ca>
2024-03-02 09:21:12 -0500

    SubClass For Warlock and Cleric

--- DnDPartyGen/Cleric.h
+++ DnDPartyGen/Cleric.h
@@ -3,5 +3,29 @@
 class Cleric :
     public Character
 {
+	
 };
 
+class Knowledge : public Cleric {
+
+};
+
+class Life : public Cleric {
+
+};
+ 
+class Light : public Cleric {
+
+};
+class Nature : public Cleric {
+
+};
+class Tempest : public Cleric {
+
+};
+class Trickery : public Cleric {
+
+};
+class War : public Cleric {
+
+};
