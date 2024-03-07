21fa4d9597019bc57da6dfe00e3f31653de98dac
Anthony Niculescu-Cornejo <antho.nicu.cor147@gmail.com>
2024-03-07 11:05:36 -0500

    ability

--- DnDPartyGen/Fighter.h
+++ DnDPartyGen/Fighter.h
@@ -4,12 +4,16 @@
 
 class Fighter : public Character
 {
+public:
+	Fighter(const Stats& charStats, int lvl, const string& name, Race _charRace, int hp,
+		const vector<Ability>& list_of_ability_core)
+		: Character(charStats, lvl, name, _charRace, hp),
+		  listOfAbilityCore(list_of_ability_core)
+	{
+	}
 
-	public :
-	
-		vector<Ability> listOfAbilityCore
+	vector<Ability> listOfAbilityCore
 		{
-
 		Ability(1, "Archery"),
 		Ability(1, "Defense"),
 		Ability(1, "Dueling"),
@@ -18,14 +22,20 @@ class Fighter : public Character
 		Ability(1, "Two-Weapon Fighting"),
 		Ability(1, "Second Wind"),
 		Ability(1, "Action Surge")
-
 		};
 };
-
 
 class BattleMaster : Fighter
 {
 public:
+	BattleMaster(const Stats& charStats, int lvl, const string& name, Race _charRace, int hp,
+		const vector<Ability>& list_of_ability_core, const vector<string>& maneuvers,
+		const vector<Ability>& list_of_ability_bm)
+		: Fighter(charStats, lvl, name, _charRace, hp, list_of_ability_core),
+		  Maneuvers(maneuvers),
+		  listOfAbilityBM(list_of_ability_bm)
+	{
+	}
 
 	vector<string> Maneuvers
 	{
@@ -37,15 +47,46 @@ public:
 	vector<Ability> listOfAbilityBM
 	{
 		Ability(3 , "Student Of War"),
-		Ability(3, "Combat Superiority"),
+		Ability(3, "Combat Superiority")
 	};
 
 };
 
 
 class Champion : Fighter
-{};
+{
+public:
+	Champion(const Stats& charStats, int lvl, const string& name, Race _charRace, int hp,
+		const vector<Ability>& list_of_ability_core, const vector<Ability>& list_of_ability_champ)
+		: Fighter(charStats, lvl, name, _charRace, hp, list_of_ability_core),
+		  listOfAbilityChamp(list_of_ability_champ)
+	{
+	}
+
+	vector<Ability> listOfAbilityChamp
+	{
+		Ability(3 , "Improved Critical"),
+		Ability(7 , "Remarkable Athlete")
+	};
+
+};
 
 
 class EldrichtKnight : Fighter
-{};
+{
+public:
+	EldrichtKnight(const Stats& charStats, int lvl, const string& name, Race _charRace, int hp,
+		const vector<Ability>& list_of_ability_core, const vector<Ability>& list_of_ability_ek)
+		: Fighter(charStats, lvl, name, _charRace, hp, list_of_ability_core),
+		  listOfAbilityEK(list_of_ability_ek)
+	{
+	}
+
+	vector<Ability> listOfAbilityEK
+	{
+		Ability(3 , "Weapon Bond"),
+		Ability(7 , "War Magic")
+	};
+
+	
+};
