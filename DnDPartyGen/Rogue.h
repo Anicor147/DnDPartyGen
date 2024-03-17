db51e3bfb39ee0cd751a35f3e114219dcbde79c2
Anicor147 <antho.nicu.cor147@gmail.com>
2024-03-17 11:13:24 -0400

    Added Rogue abilities

--- DnDPartyGen/Rogue.h
+++ DnDPartyGen/Rogue.h
@@ -1,7 +1,61 @@
 #pragma once
 #include "Character.h"
+#include <vector>
+
 class Rogue :
     public Character
 {
+public :
+    vector<Ability> listOfAbilityCore
+    {
+        Ability(1 , "Acrobatics"),
+        Ability(1 , "Athletics   "),
+        Ability(1 , " Deception"),
+        Ability(1 , "Insight"),
+        Ability(1 , " Intimidation"),
+        Ability(1 , " Perception"),
+        Ability(1 , "Performance,"),
+        Ability(1 , "Persuasion"),
+        Ability(1 , "Sleight of Hand"),
+        Ability(1 , "Stealth")
+    };
 };
 
+class Thief : Rogue
+{
+
+    vector<Ability> listOfRogueAbility
+    {
+        Ability(1 , "Fast Hand"),
+        Ability(1 , "Second Story Work"),
+        Ability(1 , "Supreme Sneak"),
+        Ability(1 , "Use Magic Device"),
+        Ability(1 , "Thief's Reflexes")
+    };
+};
+
+class Assasin: Rogue{
+
+vector<Ability> listOfAssassinAbility
+    {
+        Ability(1 , "Assasinate"),
+        Ability(1 , "Bonus Proficiencies"),
+        Ability(1 , "Infiltration Expertise"),
+        Ability(1 , "Impostor"),
+        Ability(1 , "Death Strike")
+    };
+};
+
+class AracaneTrickster : Rogue
+{
+    vector<Ability> listOfAracaneTAbility
+    {
+        Ability(1 , "SpellCasting"),
+        Ability(1 , "Mage Hand Legerdemain"),
+        Ability(1 , "Magical Ambush"),
+        Ability(1 , "Versatile Trickster"),
+        Ability(1 , "Spell Thief")
+    };
+
+
+};
